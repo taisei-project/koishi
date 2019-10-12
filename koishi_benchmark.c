edab4d52d8e8f6e4a344348febe2f34611a9fe50
Andrei Alexeyev <akari@taisei-project.org>
2019-10-12 23:01:49 +0300

    add a basic benchmark

---
+++ koishi_benchmark.c
@@ -0,0 +1,43 @@
+
+#include <koishi.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
+#define ITER 500000000
+
+static volatile int counter;
+
+void *cofunc1(void *data) {
+	while(counter < ITER) {
+		koishi_yield(data);
+	}
+
+	return NULL;
+}
+
+void *cofunc2(void *data) {
+	while(koishi_resume(data, data)) {
+		++counter;
+	}
+
+	return NULL;
+}
+
+int main(int argc, char **argv) {
+	if(argc != 1) {
+		printf("%s takes no arguments.\n", argv[0]);
+		return 1;
+	}
+
+	koishi_coroutine_t co1, co2;
+	koishi_init(&co1, 0, cofunc1);
+	koishi_init(&co2, 0, cofunc2);
+	koishi_resume(&co2, &co1);
+	koishi_deinit(&co1);
+	koishi_deinit(&co2);
+
+	printf("%i\n", counter);
+
+	return 0;
+}
