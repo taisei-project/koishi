b16c82fc6e86a1623d8ea553bfb934aee05033dc
Andrei Alexeyev <akari@taisei-project.org>
2019-10-23 14:07:14 +0300

    Add Asyncify backend for emscripten (#1)
    Unlike the 'emscripten' backend, this one doesn't use the emscripten
    coroutines API (which is currently broken) and instead implements a
    minimal context-switching "fiber" API as a JS library on top of
    Asyncify. See emscripten-core/emscripten#8979 for more details.
    
    `_koishi_impl_fiber_swap` must be manually added to `ASYNCIFY_IMPORTS`
    by the user of the library, because emcc does not provide a way to
    append to the imports array without completely overwriting it yet.

---
+++ src/asyncify/asyncify.c
@@ -0,0 +1,56 @@
+
+#include <koishi.h>
+#include <emscripten.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef void *fiber_handle_t;
+
+typedef struct asyncify_fiber {
+	fiber_handle_t handle;
+	koishi_entrypoint_t entry;
+} koishi_fiber_t;
+
+#include "../fiber.h"
+
+fiber_handle_t _koishi_impl_fiber_create(uint32_t stack_size, void (*entry)(void));
+fiber_handle_t _koishi_impl_fiber_create_for_main_ctx(void);
+void _koishi_impl_fiber_recycle(fiber_handle_t fiber, void (*entry)(void));
+void _koishi_impl_fiber_free(fiber_handle_t fiber);
+void _koishi_impl_fiber_swap(fiber_handle_t old, fiber_handle_t new);
+
+KOISHI_NORETURN static void co_entry(void) {
+	koishi_coroutine_t *co = co_current;
+	co->userdata = co->fiber.entry(co->userdata);
+	koishi_swap_coroutine(co, co->caller, KOISHI_DEAD);
+	KOISHI_UNREACHABLE;
+}
+
+static void koishi_fiber_init(koishi_fiber_t *fiber, size_t min_stack_size, koishi_entrypoint_t entry_point) {
+	size_t stack_size = koishi_util_real_stack_size(min_stack_size);
+	assert(stack_size < UINT32_MAX);
+	fiber->handle = _koishi_impl_fiber_create((uint32_t)stack_size, co_entry);
+	assert(fiber->handle != 0);
+	fiber->entry = entry_point;
+}
+
+static void koishi_fiber_deinit(koishi_fiber_t *fiber) {
+	if(fiber->handle) {
+		_koishi_impl_fiber_free(fiber->handle);
+		fiber->handle = 0;
+	}
+}
+
+static void koishi_fiber_init_main(koishi_fiber_t *fiber) {
+	fiber->handle = _koishi_impl_fiber_create_for_main_ctx();
+}
+
+static void koishi_fiber_recycle(koishi_fiber_t *fiber, koishi_entrypoint_t entry_point) {
+	_koishi_impl_fiber_recycle(fiber->handle, co_entry);
+	fiber->entry = entry_point;
+}
+
+static void koishi_fiber_swap(koishi_fiber_t *from, koishi_fiber_t *to) {
+	_koishi_impl_fiber_swap(from->handle, to->handle);
+}
---
+++ src/asyncify/library_fiber.js
@@ -0,0 +1,107 @@
+
+mergeInto(LibraryManager.library, {
+	$Fibers: {
+		swapCounter: 0,
+		continuations: {},
+	},
+
+	/*
+	 * Layout of an ASYNCIFY fiber structure
+	 *
+	 *  0 Asyncify context
+	 *  4 Stack pointer
+	 *  8 Stack max
+	 * 12 Stack:
+	 *    ...
+	 */
+	_koishi_impl_fiber__sig: 'iii',
+	_koishi_impl_fiber_create__deps: ['$Fibers', 'malloc'],
+	_koishi_impl_fiber_create: function(stack_size, funcptr) {
+		stack_size = stack_size|0;
+		funcptr = funcptr|0;
+		var fib = _malloc(stack_size + 12 | 0) | 0;
+
+		// init Asyncify context to null
+		{{{ makeSetValueAsm('fib', 0, 0, 'i32') }}};
+
+		// init stack pointer
+		{{{ makeSetValueAsm('fib', 4, '(fib+stack_size+12)', 'i32') }}};
+
+		// init STACK_MAX
+		{{{ makeSetValueAsm('fib', 8, '(fib+12)', 'i32') }}};
+
+		// init continuation (call user-supplied function)
+		Fibers.continuations[fib] = () => {
+#if STACK_OVERFLOW_CHECK
+			writeStackCookie();
+#endif
+			{{{ makeDynCall('v') }}}(funcptr);
+		};
+
+		return fib|0;
+	},
+
+	_koishi_impl_fiber_create_for_main_ctx__sig: 'i',
+	_koishi_impl_fiber_create_for_main_ctx__deps: ['$Fibers', 'malloc'],
+	_koishi_impl_fiber_create_for_main_ctx: function() {
+		var fib = _malloc(12) | 0;
+		{{{ makeSetValueAsm('fib', 8, 'STACK_MAX', 'i32') }}};
+		return fib|0;
+	},
+
+	_koishi_impl_fiber_recycle__sig: 'vii',
+	_koishi_impl_fiber_recycle__deps: ["$Fibers"],
+	_koishi_impl_fiber_recycle: function(fib, funcptr) {
+		{{{ makeSetValueAsm('fib', 0, 0, 'i32') }}};
+		{{{ makeSetValueAsm('fib', 8, '(fib+12)', 'i32') }}};
+		Fibers.continuations[fib] = () => {{{ makeDynCall('v') }}}(funcptr);
+	},
+
+	_koishi_impl_fiber_free__sig: 'vi',
+	_koishi_impl_fiber_free__deps: ["$Fibers", "free"],
+	_koishi_impl_fiber_free: function(fib) {
+		delete Fibers.continuations[fib];
+		_free(fib);
+	},
+
+	_koishi_impl_fiber_swap__sig: 'vii',
+	_koishi_impl_fiber_swap__deps: ["$Fibers"],
+	_koishi_impl_fiber_swap: function(f_old, f_new) {
+
+		return Asyncify.handleSleep((wakeUp) => {
+			var swap = () => {
+				// update asyncify context for caller
+				var asyncify_context = Asyncify.currData;
+				{{{ makeSetValueAsm('f_old', 0, 'asyncify_context', 'i32') }}};
+
+				// update stack pointer for caller
+				var stack_ptr = stackSave() | 0;
+				{{{ makeSetValueAsm('f_old', 4, 'stack_ptr', 'i32') }}};
+
+				// update continuation for caller
+				Fibers.continuations[f_old] = wakeUp;
+
+				// load asyncify context from callee
+				Asyncify.currData = {{{ makeGetValueAsm('f_new', 0, 'i32') }}};
+
+				// load stack pointer from callee
+				stack_ptr = {{{ makeGetValueAsm('f_new', 4, 'i32') }}};
+				stackRestore(stack_ptr);
+
+				// load STACK_MAX from callee
+				STACK_MAX = {{{ makeGetValueAsm('f_new', 8, 'i32') }}};
+
+				Fibers.continuations[f_new]();
+			};
+
+			if(Fibers.swapCounter == 1000) {
+				Fibers.swapCounter = 0;
+				Asyncify.afterUnwind = null;
+				setTimeout(swap);
+			} else {
+				Fibers.swapCounter++;
+				Asyncify.afterUnwind = swap;
+			}
+		});
+	},
+});
